7e20d446d52506f0b837c259cef9f8a98b131a07
Herbert Zhang <herbert.zhang@fashionsparkcn.com>
2026-07-20 14:13:22 +0800

    尝试

--- api/blob/sign.js
+++ api/blob/sign.js
@@ -77,27 +77,42 @@ export default async function handler(req,res){
 
 
 
-        // =========================
+         // =========================
         // 2. 生成临时URL
         // =========================
 
-        const presignedUrl = await presignUrl(
-            token.clientSigningToken,
+        const {
+            presignedUrl
+        } =
+        await presignUrl(
+
+            token,
+
             {
+
                 pathname: pathname,
 
-                operation: "get",
+                operation:"get",
 
                 access:"private",
 
+                // 10分钟有效
+
                 validUntil:
-                    Date.now() + 10 * 60 * 1000
+                Date.now() + 
+                10 * 60 * 1000
+
             }
+
         );
 
 
+
+
         return res.status(200).json({
-            url: presignedUrl
+
+            url:presignedUrl
+
         });
 
 
